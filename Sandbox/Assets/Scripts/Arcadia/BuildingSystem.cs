3ac1df6804fcb549d08fbcadd6eae1b285fe539c
Dread5 <juguterr@gmail.com>
2021-11-25 18:27:50 -0600

    Basic building system introduced
    Added a basic grid-based building system.
    
    Did some clean-up with some code and minor additions

--- Sandbox/Assets/Scripts/Arcadia/BuildingSystem.cs
+++ Sandbox/Assets/Scripts/Arcadia/BuildingSystem.cs
@@ -5,95 +5,52 @@ using UnityEngine;
 public class BuildingSystem : MonoBehaviour
 {
     public GameObject buildingPrefab;
+    public Material previewMaterial;
 
-    public float gridDistance = 2f;
+    [SerializeField]
+    LayerMask groundMask;
 
-    public LayerMask buildLayer;
-
-    float gridSize;
+    Material builtMaterial;
 
     GameObject previewPrefab;
 
     Vector3 gridPosition;
-    Vector3 worldPosition;
 
     void Start()
     {
-        worldPosition = transform.position;
-
-        gridSize = 1f / gridDistance;
-
         previewPrefab = Instantiate(buildingPrefab, buildingPrefab.transform.position, Quaternion.identity);
-
         previewPrefab.GetComponent<BoxCollider>().enabled = false;
+
+        builtMaterial = buildingPrefab.GetComponent<Renderer>().material;
+        previewPrefab.GetComponent<Renderer>().material = previewMaterial;
+
+        buildingPrefab.SetActive(false);
     }
 
     void Update()
     {
-        gridSize = 1f / gridDistance;
-
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
 
         RaycastHit hit;
 
-        Physics.Raycast(ray, out hit, 6f, buildLayer);
+        Physics.Raycast(ray, out hit, 6f, groundMask);
 
-        worldPosition = hit.point + hit.normal.normalized;
+        Debug.DrawLine(Camera.main.transform.position, hit.point, Color.red);
 
-        //Vector3 distanceVec = hit.point - Camera.main.transform.position;
-        //worldPosition -= distanceVec.normalized * (distanceVec.magnitude - 1f);
+        gridPosition = hit.point + (hit.normal / 2f);
 
         gridPosition = new Vector3(
-        Mathf.Round(worldPosition.x * gridSize) / gridSize,
-        Mathf.Round(worldPosition.y * gridSize) / gridSize,
-        Mathf.Round(worldPosition.z * gridSize) / gridSize);
-
-        //Vector3 newPos = hit.point + hit.normal.normalized;
-
-        //gridPosition += newPos;
-
-        transform.position = gridPosition;
+            Mathf.Round(gridPosition.x),
+            Mathf.Round(gridPosition.y),
+            Mathf.Round(gridPosition.z));
 
         previewPrefab.transform.position = gridPosition;
 
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(buildingPrefab, gridPosition, Quaternion.identity);
+            GameObject createdObject = Instantiate(previewPrefab, gridPosition, Quaternion.identity);
+            createdObject.GetComponent<BoxCollider>().enabled = true;
+            createdObject.GetComponent<Renderer>().material = builtMaterial;
         }
     }
-
-    /*
-    [SerializeField] GameObject selectedObject;
-
-    [SerializeField] Transform rayCastOrigin;
-
-    [SerializeField]
-    LayerMask buildLayer;
-
-    RaycastHit hit;
-    Vector3 buildPosition;
-
-    void Update()
-    {
-        if(Physics.Raycast(rayCastOrigin.position, rayCastOrigin.forward, out hit, 6f, buildLayer))
-        {
-            if(hit.transform != this.transform)
-            {
-                previewObject(hit);
-            }
-        }
-    }
-
-    void previewObject(RaycastHit hit2)
-    {
-        float step = 10f * Time.deltaTime;
-        buildPosition = new Vector3(Mathf.RoundToInt(hit.point.x), hit.point.y, Mathf.RoundToInt(hit.point.z));
-        selectedObject.transform.position = buildPosition; //Vector3.MoveTowards(selectedObject.transform.position, buildPosition, step);
-
-        if (Vector3.Distance(transform.position, selectedObject.transform.position) < 0.001f)
-        {
-            selectedObject.transform.position *= -1.0f;
-        }
-    }
-    */
 }
